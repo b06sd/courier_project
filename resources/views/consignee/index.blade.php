a9ed3ca7c6f050e8d89ba1a4a6dca6484ce93605
ridwanm <ridwan554@ymail.com>
2018-07-13 10:09:37 +0100

    minor changes in consignee index

--- resources/views/consignee/index.blade.php
+++ resources/views/consignee/index.blade.php
@@ -7,10 +7,10 @@
 <!-- Bread crumb -->
 <div class="row page-titles">
 	<div class="col-md-5 align-self-center">
-		<h3 class="text-primary">User Management</h3> </div>
+		<h3 class="text-primary">Logistic Management</h3> </div>
 		<div class="col-md-7 align-self-center">
 			<ol class="breadcrumb">
-				<li class="breadcrumb-item"><a href="javascript:void(0)">Manage Users</a></li>
+				<li class="breadcrumb-item"><a href="javascript:void(0)">Manage Consignee</a></li>
 				<li class="breadcrumb-item active">Dashboard</li>
 			</ol>
 		</div>
