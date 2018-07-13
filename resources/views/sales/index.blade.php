dfac62cc3febd16dedcb2f4ededa348fba7646ce
Oladotun Denton <dotundenton@gmail.com>
2018-07-13 12:09:51 +0100

    Renamed SalesPersonController to SalesController, Modified _partials/sidebar to reflect the change from SalesPerson to Sales. Renamed SalesPerson view directory to Sales and Adjusted the routes/web.php to reflect the changes in the SalesController and the sales view directory

--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -7,7 +7,7 @@
     <h3 class="text-primary">Customer Management</h3> </div>
     <div class="col-md-7 align-self-center">
       <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="javascript:void(0)">Manage Sales Person</a></li>
+        <li class="breadcrumb-item"><a href="javascript:void(0)">Manage Sales</a></li>
         <li class="breadcrumb-item active">Dashboard</li>
       </ol>
     </div>
