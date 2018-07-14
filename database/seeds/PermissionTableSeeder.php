50d86d51822a46f35769892abe8135230dc71377
kingsley Udenewu <kingzpacking@gmail.com>
2018-07-14 18:51:08 +0100

    Added role seed and made seeding auto generation

--- database/seeds/PermissionTableSeeder.php
+++ database/seeds/PermissionTableSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class PermissionTableSeeder extends Seeder
 {
